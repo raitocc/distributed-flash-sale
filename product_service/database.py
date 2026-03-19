79f5c940af0b5731bf25b22b48768edc28333448
raitocc <raitocc@qq.com>
2026-03-19 17:39:19 +0800

    feat(database): 商品服务添加数据库自动创建功能

--- product_service/database.py
+++ product_service/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 from config import settings
 
 engine = create_engine(settings.database_url)
+
+# 程序启动时检查，如果没有 flash_order_db 这个库，就自动建一个！
+if not database_exists(engine.url):
+    create_database(engine.url)
+    print(f"数据库 {engine.url.database} 已自动创建成功！")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
